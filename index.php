c6761bb477c95a4e4e7826ec8f16af6f7f053707
Alexey <kovalevskiy2011@gmail.com>
2019-07-31 23:56:08 +0300

    Added empty files

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta name="Comfrotel" content="text/html" charset="utf-8">
+  <title>Comfrotel Exercise</title>
+  <meta name="description" content="Тестовое упражнение">
+</head>
+<body>
+  <header>
+    <h1>Упражнение Comfrotel</h1>
+  </header>
+
+  <main>
+
+  </main>
+
+  <footer>
+
+  </footer>
+</body>
