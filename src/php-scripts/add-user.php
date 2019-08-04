f905131000be763fa46f15f2e5985be497c54184
Alexey <kovalevskiy2011@gmail.com>
2019-08-04 18:50:02 +0300

    Доработана админка, добавленны операции Редактирования/Добавления

---
+++ src/php-scripts/add-user.php
@@ -0,0 +1,18 @@
+<?php
+require_once 'db-connect.php';
+if (!empty($_POST))
+{
+ $query = "
+   INSERT INTO `users` (`id`, `last_name`, `name`, `third_name`, `status`)
+   VALUES (NULL, '".str_replace(' ', '', $_POST['last_name'])."', '".str_replace(' ', '', $_POST['name'])."', '".str_replace(' ', '', $_POST['third_name'])."', '".str_replace(' ', '', $_POST['status'])."')
+  ";
+  echo $query;
+  $result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link));
+  if(!$result)
+  {
+    echo "Ошибка добавления";
+  }
+  mysqli_close($link);
+}
+
+?>
--- src/php-scripts/admin.php
+++ src/php-scripts/admin.php
@@ -3,4 +3,40 @@
   <span>Назад</span>
 </div>
 <span>Вы администратор</span>
+<br> <button type="button" name="add-user" onclick="ShowAdder(false)">Добавить клиента</button>
+<div class="shadow">
+  <div class="adder-block block">
+    <div class="center">
+      <form class="add-user" id="#form-add-user" action="javascript:void(null);" method="post" onsubmit="add_user()">
+        <br>Фамилия: <input type="text" name="last_name" value="">
+        <br>Имя: <input type="text" name="name" value="">
+        <br>Отчество: <input type="text" name="third_name" value="">
+        <br> Статус: <select name="status">
+              <option value="Первый">Первый</option>
+              <option value="Второй">Второй</option>
+              <option value="Третий">Третий</option>
+        </select>
+        <br><input type="submit" name="add" value="Добавить">
+      </form>
+      <form class="edit-user" id="#form-edit-user" action="javascript:void(null);" method="post" onsubmit="edit_user_ok()">
+        <input type="text" name="id_edit" value="" style="display:none;">
+        <br>Фамилия: <input type="text" name="last_name_edit" value="">
+        <br>Имя: <input type="text" name="name_edit" value="">
+        <br>Отчество: <input type="text" name="third_name_edit" value="">
+        <br> Статус: <select name="status_edit">
+              <option value="Первый">Первый</option>
+              <option value="Второй">Второй</option>
+              <option value="Третий">Третий</option>
+        </select>
+        <br>Удалить?<input type="checkbox" name="delete_edit" value="">
+        <br><input type="submit" name="edit" value="Ок">
+      </form>
+    </div>
+  </div>
+</div>
+<div class="user-table">
+  <?php
+    require 'query-table-admin.php';
+   ?>
+</div>
 </main>
